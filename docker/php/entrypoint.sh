bbe1db2782fabca6231d6575fc1bb9048368c3bc
Marius Tamazlicaru <mtamazlicaru@pentalog.fr>
2017-07-04 18:10:23 +0300

    Init docker config.

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if [ -z "$DISABLE_PING" -o "$DISABLE_PING" = "false" ]; then
+  until nc -z -v -w90 $SYMFONY__DATABASE__HOST $SYMFONY__DATABASE__PORT
+  do
+    echo "Waiting for database connection..."
+    sleep 5
+  done
+fi
+
+composer install
+
+#bin/console doctrine:database:create --if-not-exists
+#bin/console doctrine:migration:migrate --no-interaction
+#bin/console doctrine:fixtures:load --fixtures=tests/DataFixtures/ORM --no-interaction
+
+chown -R www-data:www-data var/cache var/logs
+
+exec "$@"
