75907ca167494b79007994d74dc557197acd7f34
Nicoleta LAPTEDULCE <nlaptedulce@pentalog.fr>
2017-07-06 16:57:03 +0300

    Creating entities and Fixtures
    (cherry picked from commit e806b9d)

---
+++ src/AppBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Category;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+
+class LoadCategoryData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    { for ($i = 0; $i < 10; $i++){
+            $faker = Factory::create();
+            $category = new Category();
+            $category->setTitle($faker->title);
+            $manager->persist($category);
+
+    }
+
+    $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 1;
+    }
+
+}
---
+++ src/AppBundle/DataFixtures/ORM/LoadQuizData.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use AppBundle\Entity\Quiz;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+
+class LoadQuizData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $faker = Factory::create();
+
+            $quiz = new Quiz();
+            $quiz->setTitle($faker->title);
+            $quiz->setCreatedAt($faker->dateTime);
+            $quiz->setDescription($faker->text);
+            $manager->persist($quiz);
+        }
+
+        $manager->flush();
+    }
+
+}
