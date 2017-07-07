69dcc0563036e98bbf9100823df8a8d0bdfa6f59
atimchiv <atimchiv@pentalog.fr>
2017-07-07 13:42:18 +0000

    Result / ResultAnswer fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadResultAnswerData.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\ResultAnswer;
+use AppBundle\Entity\Result;
+use AppBundle\Entity\Question;
+use AppBundle\Entity\Answer;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class LoadResultAnswerData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+ * @param ObjectManager $manager
+ */
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = Factory::create();
+        $results = $manager->getRepository(Result::class)->findAll();
+        $questions = $manager->getRepository(Question::class)->findAll();
+        $answers = $manager->getRepository(Answer::class)->findAll();
+
+
+        for ($i = 0; $i < 10; $i++) {
+            $resultAnswer = new ResultAnswer();
+            $resultAnswer->setResult($faker->randomElement($results));
+            $resultAnswer->setQuestion($faker->randomElement($questions));
+            $resultAnswer->setAnswer($faker->randomElement($answers));
+
+            $manager->persist($resultAnswer);
+        }
+
+        $manager->flush();
+
+}
+
+
+public function getOrder()
+{
+    return 4;
+}
+}
