14f723d14902347eefa32d9f31024568649f89cc
nlaptedulce <nlaptedulce@pentalog.fr>
2017-07-10 11:26:16 +0300

    Install bootstrap and jquery

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,11 +2,8 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -16,17 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $user = new User();
-        $user->setUsername("");
-        $user->setPassword("");
-        $form = $this->createFormBuilder($user)
-            ->add('username', TextType::class)
-            ->add('password', TextType::class)
-            ->add('save', SubmitType::class, array('label' => 'Login'))
-            ->getForm();
-
-        return $this->render('default/index.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
