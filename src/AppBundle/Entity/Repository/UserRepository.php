e9a558a296f5b28c4ba8d4be7ab279ac65227275
atimchiv <atimchiv@pentalog.fr>
2017-07-10 17:56:34 +0300

    broken version ~30% shall be wrong , please show me where to correct/add code

--- src/AppBundle/Entity/Repository/UserRepository.php
+++ src/AppBundle/Entity/Repository/UserRepository.php
@@ -1,10 +1,47 @@
 <?php
 
 namespace AppBundle\Entity\Repository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
+    /**
+     * getAllUsers() method
+     *
+     * @param integer $currentPage The current page (passed from controller)
+     *
+     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     */
+    public function getAllUsers($currentPage = 1)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->orderBy('p.created', 'DESC')
+            ->getQuery();
+
+        $paginator = $this->paginate($query, $currentPage);
+
+        return $paginator;
+    }
+
+    /**
+     * @param Doctrine\ORM\Query $dql   DQL Query Object
+     * @param integer            $page  Current page (defaults to 1)
+     * @param integer            $limit The total number per page (defaults to 19)
+     *
+     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     */
+    public function paginate($dql, $page = 1, $limit = 19)
+    {
+        $paginator = new Paginator($dql);
+
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1)) // Offset
+            ->setMaxResults($limit); // Limit
+
+        return $paginator;
+    }
+
 
 }
