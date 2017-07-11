8f2a9a4916304d7399ecbfac97da8b749fda8a32
atimchiv <atimchiv@pentalog.fr>
2017-07-11 14:54:36 +0300

    code style things

--- src/AppBundle/Entity/Repository/UserRepository.php
+++ src/AppBundle/Entity/Repository/UserRepository.php
@@ -4,17 +4,14 @@ namespace AppBundle\Entity\Repository;
 
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\Pagination\Paginator;
-
 use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
     /**
-     * getAllUsers() method
+     * @param int $currentPage
      *
-     * @param integer $currentPage The current page (passed from controller)
-     *
-     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     * @return Paginator
      */
     public function getAllUsers($currentPage = 1)
     {
@@ -22,25 +19,23 @@ class UserRepository extends EntityRepository
             ->orderBy('u.createdAt', 'DESC')
             ->getQuery();
 
-        $paginator = $this->paginate($query, $currentPage);
-
-        return $paginator;
+        return $this->paginate($query, $currentPage);
     }
 
     /**
-     * @param Query $dql   DQL Query Object
-     * @param integer            $page  Current page (defaults to 1)
-     * @param integer            $limit The total number per page (defaults to 19)
+     * @param Query $dql
+     * @param int $page
+     * @param int $limit
      *
-     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     * @return Paginator
      */
     public function paginate($dql, $page = 1, $limit = 19)
     {
         $paginator = new Paginator($dql);
 
         $paginator->getQuery()
-            ->setFirstResult($limit * ($page - 1)) // Offset
-            ->setMaxResults($limit); // Limit
+            ->setFirstResult($limit * ($page - 1))
+            ->setMaxResults($limit);
 
         return $paginator;
     }
