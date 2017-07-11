2217ad5304029c7eea20650b4591ae0bd8a040e6
pbujac <pbujac@pentalog.fr>
2017-07-11 15:30:46 +0300

    added bcrypt, deleted UserManager class, change UserFixtures

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -7,6 +7,7 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\User;
 use Faker\Factory;
+use Faker\Generator;
 
 class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
 {
@@ -20,7 +21,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setUsername($faker->userName);
-            $user->setPassword($faker->password());
+            $user->setPassword(password_hash('test', PASSWORD_BCRYPT));
             $user->setActive($faker->boolean());
             $user->setCreatedAt(
                 $faker->dateTimeBetween('-3 years', 'now')
@@ -32,7 +33,29 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
             $manager->persist($user);
         }
 
+        $this->createAdminUser($faker, $manager);
+
         $manager->flush();
+    }
+
+    /**
+     * @param Generator $faker
+     * @param ObjectManager $manager
+     */
+    private function createAdminUser($faker, ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setPassword(password_hash('admin', PASSWORD_BCRYPT));
+        $user->setActive(1);
+        $user->setCreatedAt(
+            $faker->dateTimeBetween('-3 years', 'now')
+        );
+        $user->setFirstName($faker->firstName);
+        $user->setLastName($faker->lastName);
+        $user->addRole(User::ROLE_ADMIN);
+
+        $manager->persist($user);
     }
 
     public function getOrder()
