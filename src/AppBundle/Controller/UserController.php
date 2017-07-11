6364f3abf4b459a82e65d5286fd06dd973c7bf1b
atimchiv <atimchiv@pentalog.fr>
2017-07-11 17:20:45 +0300

    test

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,26 +5,22 @@ use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
-class ListOfUsersController extends DefaultController
+class UserController extends DefaultController
 {
     /**
-     * @Route("/userList/{page}",name="user_index")
+     * @Route("/userList/{page}",name="user_list_page")
      */
-    public function indexAction($page=1)
+    public function userListAction($page=1)
     {
-        $repository = $this->getDoctrine()
-            ->getRepository(User::class);
-        $users = $repository->getAllUsers($page); // Returns 19 users out of 20
+        $users = $this->getDoctrine()->getRepository(User::class)->getAllUsers($page);
 
         $limit = 19;
         $maxPages = ceil($users->count() / $limit);
-        $thisPage = $page;
 
         return $this->render('userList/list.html.twig', [
             'users' => $users->getQuery()->getResult(),
             'maxPages' => $maxPages,
-            'thisPage' => $thisPage,
+            'thisPage' => $page,
         ]);
     }
-
 }
