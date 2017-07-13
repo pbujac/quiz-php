866073ac49105e041f246726437f031a8c7b54ed
atimchiv <atimchiv@pentalog.fr>
2017-07-13 16:27:13 +0300

    test

---
+++ src/AdminBundle/Validator/CategoryValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AdminBundle\Validator;
+
+use AppBundle\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class CategoryValidator extends ConstraintValidator
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param string $value
+     * @param Constraint $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        $repository = $this->em->getRepository(Category::class);
+        $category = $repository->findOneBy([
+            'title' => $value
+        ]);
+
+        if ($category) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
