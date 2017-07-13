80cc77ec8f5009b5cee4a03d63c0c665707186fd
atimchiv <atimchiv@pentalog.fr>
2017-07-13 17:04:00 +0300

    fixed pagination on users list

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -2,30 +2,35 @@
 
 namespace AdminBundle\Controller;
 
+use AdminBundle\Manager\PaginatorManager;
 use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     /**
-     * @Route("/user",name="admin.user.list")
+     * @param int $page = 1
      *
+     * @return RedirectResponse|Response
+     *
+     * @Route("/user/{page}",name="admin.user.list")
      */
-    public function userListAction(Request $request)
+    public function userListAction(Request $request, int $page = 1)
     {
-        $page = $request->get('page', 1);
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->getAllUsersByPage($page);
 
-        $users = $this->getDoctrine()->getRepository(User::class)->getAllUsers($page);
+        $maxPages = ceil($users->count() / PaginatorManager::PAGE_LIMIT);
 
-        $limit = 19;
-        $maxPages = ceil($users->count() / $limit);
-
-        return $this->render('userList/list.html.twig', [
+        return $this->render('admin/user/list.html.twig', [
             'users' => $users->getQuery()->getResult(),
             'maxPages' => $maxPages,
-            'thisPage' => $page,
+            'currentPage' => $page,
         ]);
     }
 }
