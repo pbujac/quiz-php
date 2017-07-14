9e3e0c6c0dd8c4ebd8cadd6ed94784296a084f66
nlaptedulce <nlaptedulce@pentalog.fr>
2017-07-14 10:48:04 +0300

    Edit User

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -52,5 +52,45 @@ class UserController extends Controller
             ]);
     }
 
+    /**
+     * @param User $user
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     *
+     * @Route("/user/edit/{user}", name="admin.user.edit")
+     */
+    public function editAction(User $user,Request $request)
+    {
+        $form = $this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setCreatedAtValue();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                $user->getUsername() . ' user was modified!'
+            );
+
+
+            return $this->redirectToRoute('admin.user.edit');
+        }
+
+        return $this->render(
+            'admin/user/edit.html.twig',
+            ['form' => $form->createView()
+            ]);
+    }
+
 }
 
