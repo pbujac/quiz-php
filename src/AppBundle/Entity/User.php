2adb6cb9bc2675a2bf4337a31769bce58ade8206
atimchiv <atimchiv@pentalog.fr>
2017-07-17 17:07:19 +0300

    same

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,7 +5,6 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
