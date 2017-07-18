dda370102b56c1dcce429f8da8ffdcba7d543ef9
nlaptedulce <nlaptedulce@pentalog.fr>
2017-07-18 10:16:11 +0300

    Edit user, change param

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -37,18 +37,21 @@ class UserController extends Controller
     }
 
     /**
-     * @param User $user_id
+     * @param User $user
      *
      * @return RedirectResponse|Response
      *
      * @Route("/user/{user_id}/enable",name="admin.user.enable")
+     *
+     * @ParamConverter("user", options={"id" = "user_id"})
+     *
      */
-    public function enableAction(User $user_id)
+    public function enableAction(User $user)
     {
-        $user_id->setActive(!$user_id->isActive());
+        $user->setActive(!$user->isActive());
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($user_id);
+        $em->persist($user);
         $em->flush();
 
         return $this->redirectToRoute('admin.user.list');
