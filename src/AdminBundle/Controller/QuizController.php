00f054c425df8a20e015bbb7230f90a91b71cb96
atimchiv <atimchiv@pentalog.fr>
2017-07-20 16:08:35 +0300

    last test

--- src/AdminBundle/Controller/QuizController.php
+++ src/AdminBundle/Controller/QuizController.php
@@ -27,7 +27,7 @@ class QuizController extends Controller
 
         $quizzes = $this->getDoctrine()
             ->getRepository(Quiz::class)
-            ->getByFilter($filter ,$page);
+            ->getQuizByFilter($filter ,$page);
 
         $maxPages = ceil($quizzes->count() / PaginatorManager::PAGE_LIMIT);
 
