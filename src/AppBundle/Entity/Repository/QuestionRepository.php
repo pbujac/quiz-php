326ea86a02684c6d81a0f20db9ee778db1d80d42
pbujac <pbujac@pentalog.fr>
2017-07-20 17:16:25 +0300

    removed unused methods from repository

--- src/AppBundle/Entity/Repository/QuestionRepository.php
+++ src/AppBundle/Entity/Repository/QuestionRepository.php
@@ -6,14 +6,4 @@ use Doctrine\ORM\EntityRepository;
 
 class QuestionRepository extends EntityRepository
 {
-    public function countQuestionsByQuizId(int $quizId)
-    {
-        return $this->createQueryBuilder('question')
-            ->select('count(question.id)')
-            ->join('question.quiz', 'quiz')
-            ->where('quiz.id = :quizId')
-            ->setParameter('quizId', $quizId)
-            ->getQuery()
-            ->getSingleScalarResult();
-    }
 }
