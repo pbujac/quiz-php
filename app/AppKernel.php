ccc97f91fb8dd2a790bd2a9905997d4f317fa8b1
nlaptedulce <nlaptedulce@pentalog.fr>
2017-07-21 11:43:17 +0300

    Add JMS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
             new AdminBundle\AdminBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new ApiBundle\ApiBundle(),
         ];
 
