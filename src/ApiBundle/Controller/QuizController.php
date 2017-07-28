9c556b5f153b78e1803bf6a940a97e3acb542ddc
atimchiv <atimchiv@pentalog.fr>
2017-07-28 17:50:15 +0300

    current ver , with no useless things

--- src/ApiBundle/Controller/QuizController.php
+++ src/ApiBundle/Controller/QuizController.php
@@ -2,12 +2,7 @@
 
 namespace ApiBundle\Controller;
 
-use AppBundle\Entity\Quiz;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\BrowserKit\Response;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Rest\Route("/quizzes")
@@ -17,27 +12,12 @@ class QuizController
 {
     /**
      * @Rest\Get("/{filter}")
-     *
-     * @param Request $request
-     * @param int $page
-     *
-     * @return
      */
-    public function quizSearch(Request $request, int $page = 1)
+    public function quizSearch()
     {
-//        $filter = $request->get('filter');
-//
-//        $quizzes = $this->getDoctrine()
-//            ->getRepository(Quiz::class)
-//            ->getQuizByFilter($filter, $page);
-//
-//        $maxPages = ceil($quizzes->count() / PaginatorManager::PAGE_LIMIT);
-//
-//        return $this->render('admin/quiz/list.html.twig', [
-//            'quizzes' => $quizzes->getQuery()->getResult(),
-//            'maxPages' => $maxPages,
-//            'currentPage' => $page,
-//        ]);
+
+
+
     }
 
 
