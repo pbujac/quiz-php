be77cfc30408771e5a29fbc680126132e7d35743
nlaptedulce <nlaptedulce@pentalog.fr>
2017-07-31 15:24:11 +0300

    get category with pagination-resolve conflict

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -11,7 +11,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfonay\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
