7685501808db9119e5dd692508e477074749fc13
vvacarov <vvacarov@gmail.com>
2017-07-31 14:24:04 +0300

    add a write use to quizController

--- src/ApiBundle/Controller/QuizController.php
+++ src/ApiBundle/Controller/QuizController.php
@@ -2,26 +2,28 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\DTO\QuizDTO;
 use ApiBundle\Handler\QuizHandler;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
 
 class QuizController extends FOSRestController
 {
-//    /**
-//     * @Rest\Post("/quizzes", name="quizzes.create")
-//     *
-//     * @param QuizDTO $quizDTO
-//     *
-//     * @return View
-//     */
-//    public function postAction(QuizDTO $quizDTO)
-//    {
-//        $this->get(QuizHandler::class)->handleCreate($quizDTO);
-//
-//        return View::create($quizDTO, Response::HTTP_CREATED);
-//    }
+    /**
+     * @Rest\Post("/quizzes", name="quizzes.create")
+     *
+     * @param QuizDTO $quizDTO
+     *
+     * @return View
+     */
+    public function postAction(QuizDTO $quizDTO)
+    {
+        $this->get(QuizHandler::class)->handleCreate($quizDTO);
+
+        return View::create($quizDTO, Response::HTTP_CREATED);
+    }
 
     /**
      * @Rest\Get("/quiz/{quizId}", name="quiz.id")
